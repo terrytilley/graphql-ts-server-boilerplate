a746e15b188216675ed97e7cee7300a7ab57a976
Terry Tilley <terry@terrytilley.com>
2018-04-28 16:02:45 +0100

    setup tests with jest :black_joker:

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
+import * as path from 'path';
+import { createConnection } from 'typeorm';
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
-import { createConnection } from 'typeorm';
 import { resolvers } from './resolvers';
-import * as path from 'path';
 
-const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
+export const startServer = async () => {
+  const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
+  const server = new GraphQLServer({ typeDefs, resolvers });
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+  await createConnection();
+  await server.start();
 
-createConnection().then(() => {
-  server.start(() => console.log('Server is running on localhost:4000'));
-});
+  console.log('Server is running on localhost:4000');
+};
+
+startServer();
